29003407cba44490d93e6ba0237332ac86415c1f
PhilippRos <phi.rostock.25@lehre.mosbach.dhbw.de>
2026-09-21 11:49:56 +0200

    Die Änderungen die wir zusammen gemacht haben

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,123 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const name = ref('')
+const essenDabei = ref('')
+const essenHolen = ref('')
+const welcherDoener = ref('')
+const andererDoener = ref('')
+const essensOrt = ref('')
+const planungen = ref([])
+
+function planungSpeichern() {
+  const planung = {
+    name: name.value,
+    essenDabei: essenDabei.value,
+    essenHolen: essenHolen.value,
+    welcherDoener: welcherDoener.value,
+    andererDoener: andererDoener.value,
+    essensOrt: essensOrt.value
+  }
+
+  planungen.value.push(planung)
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <form @submit.prevent="planungSpeichern">
+      <h1>Mittagspause</h1>
+      <p>Plane deine Mittagspause mit dem Kurs.</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <label for="name">Name:</label>
+      <input id="name" v-model.trim="name" type="text" placeholder="Dein Name" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <p v-if="name">Hallo {{ name }}</p>
+
+      <p>Essen dabei?</p>
+
+      <label>
+        <input type="radio" value="ja" v-model="essenDabei" />
+        Ja
+      </label>
+
+      <label>
+        <input type="radio" value="nein" v-model="essenDabei" />
+        Nein
+      </label>
+
+      <div v-if="essenDabei === 'nein'">
+        <label for="essenHolen">Was möchtest du dir holen?</label>
+
+        <select id="essenHolen" v-model="essenHolen">
+          <option value="">Bitte auswählen</option>
+          <option value="rewe">Rewe</option>
+          <option value="doener">Döner</option>
+          <option value="baecker">Bäcker</option>
+          <option value="megges">Megges</option>
+        </select>
+
+        <div v-if="essenHolen === 'doener'">
+          <label for="welcherDoener">Welcher Döner?</label>
+
+          <select id="welcherDoener" v-model="welcherDoener">
+            <option value="">Bitte auswählen</option>
+            <option value="kebup">Kebup</option>
+            <option value="munzur">Munzur Kebaphaus</option>
+            <option value="king">King Mese Döner</option>
+            <option value="nudelhaus">Nudelhaus</option>
+            <option value="anderer">Anderer Dönerladen</option>
+          </select>
+
+          <div v-if="welcherDoener === 'anderer'">
+            <label for="andererDoener">Anderer Dönerladen:</label>
+
+            <input id="andererDoener" v-model.trim="andererDoener" type="text" placeholder="Anderer Dönerladen" />
+          </div>
+        </div>
+      </div>
+
+      <p>Wo möchtest du essen?</p>
+
+      <select id="essensOrt" v-model="essensOrt">
+        <option value="">Bitte auswählen</option>
+        <option value="raum">Im Raum</option>
+        <option value="kosmos">Kosmos</option>
+        <option value="draussen">irgendwo draußen</option>
+        <option value="marvin">bei Marvin</option>
+
+      </select>
+
+      <button type="submit">Planung speichern</button>
+    </form>
+
+    <h2>Planungen</h2>
+
+    <div v-for="planung in planungen" :key="planung.name">
+      <p>
+        {{ planung.name }} -
+        Essen dabei: {{ planung.essenDabei }} -
+      <div v-if="essenDabei === 'nein'">
+        Essen holen bei: {{ planung.essenHolen }}
+      </div>
+      <div v-if="essenHolen === 'doener'">
+
+        <div v-if="welcherDoener === 'anderer'">
+          ({{ planung.andererDoener }})
+        </div>
+        <div v-else>
+          ({{ planung.welcherDoener }})
+        </div>
+
+
+
+      </div>
+      Ort: {{ planung.essensOrt }}
+      </p>
     </div>
-  </header>
 
-  <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
